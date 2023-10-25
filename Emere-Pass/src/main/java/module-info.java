412f05d945eea3cdfb1b0c988edb4a98f4ac0301
Powergu1d0 <kalwardin739@gmail.com>
2023-10-25 10:26:34 -0300

    Uma renca de bagaça

--- Emere-Pass/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.example.javafxp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.example.javafxproject to javafx.fxml;
-    exports com.example.javafxproject;
-}
